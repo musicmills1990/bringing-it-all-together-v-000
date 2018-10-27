97627245b7a33cfa40abc9264442117d612240eb
Marc Mills <mdmills1990@gmail.com>
2018-10-27 17:34:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -78,6 +78,10 @@ class Dog
       end.first
   end
 
+    self.new(id, name, breed)
+  end
+
+
   def self.find_by_id(id)
     sql = <<-SQL
     SELECT *
@@ -89,6 +93,29 @@ class Dog
     DB[:conn].execute(sql, id).collect do |row|
     self.new_from_db(row)
     end.first
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?", name, breed)
+    if !dog.empty?
+      dog = Dog.new(dog[0][0], dog[0][1], dog[0][2])
+    else
+      dog = self.create(name: name, album: album)
+    end
+    dog
+  end
+
+  def self.find_by_name
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
   end
 
   def update
